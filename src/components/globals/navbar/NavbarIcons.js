b9b5780a10291863f4351550e09a332cdaa735f8
scottdiemer <sdiemer@cfl.rr.com>
2019-04-28 23:06:55 -0400

    implemented sass and built components of Navbar

---
+++ src/components/globals/navbar/NavbarIcons.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+class NavbarIcons extends Component {
+  render() {
+    return <div>content from navbar icons</div>
+  }
+}
+
+export default NavbarIcons
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,50 +1,34 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
-import "./layout.css"
+import { createGlobalStyle } from "styled-components"
+import Navbar from "../components/globals/navbar"
+// import "./bootstrap.min.css"
+// import "./layout.css"
+// import "../sass/layout.scss"
 
-const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
-          }
-        }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
-)
+const Layout = ({ children }) => {
+  return (
+    <React.Fragment>
+      <GlobalStyle />
+      <Navbar />
+      {children}
+    </React.Fragment>
+  )
+}
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Open Sans', sans-seriff;
+  color: #262626;
+  background: white;
+}
+`
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
